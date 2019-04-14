3590a58e7a620ed9182e09879786507c56af70a5
Taidaigh <pierre.dealmeida@yahoo.fr>
2019-04-14 13:54:30 +0200

    Reset

--- resources/[esx]/[base]/esx_optionalneeds/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.TickTime         = 100
-Config.UpdateClientTime = 6000
-Config.Locale 			= 'fr'
--- resources/[esx]/[base]/esx_status/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.StatusMax      = 1000000
-Config.TickTime       = 1500
-Config.UpdateInterval = 12000
